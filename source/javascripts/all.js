3ddb9ecda52665a5097680178ab09b4e2bd3924b
andklar <andrewklar@hotmail.com>
2016-07-16 17:00:04 -0400

    textbox

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,41 +7,41 @@
         //if the current image is picture1.png, change it to picture2.png
         if(src == 'images/angry-256.png') {
             $("#angry").attr("src","images/quiet-256.png");
-            $('.textbox').append('<p class="floatright">Go To Quiet Place</p>');
+            $('.textbox1').append('<p class="floatright">Go To Quiet Place</p>');
         }
         else if(src == 'images/quiet-256.png') {
-            $('.textbox').html('');
+            $('.textbox1').html('');
             $("#angry").attr("src","images/meditation-256.png");
-            $('.textbox').append('<p class="floatright">Try Meditation</p>');
+            $('.textbox1').append('<p class="floatright">Try Meditation</p>');
 
         } else if(src == "images/meditation-256.png") {
-          $('.textbox').html('');
+          $('.textbox1').html('');
             $("#angry").attr("src","images/teddy-bear-256.png");
-            $('.textbox').append('<p class="floatright">Give Them Sensory Toys</p>');
+            $('.textbox1').append('<p class="floatright">Give Them Sensory Toys</p>');
 
         } else if(src == "images/teddy-bear-256.png") {
-          $('.textbox').html('');
+          $('.textbox1').html('');
             $("#angry").attr("src","images/hug-256.png");
-            $('.textbox').append('<p class="floatright">Gently Rock Back and Fourth</p>');
+            $('.textbox1').append('<p class="floatright">Gently Rock Back and Fourth</p>');
 
         } else if(src == "images/hug-256.png") {
-          $('.textbox').html('');
+          $('.textbox1').html('');
             $("#angry").attr("src","images/television-256.png");
-            $('.textbox').append('<p class="floatright">Watch TV</p>');
+            $('.textbox1').append('<p class="floatright">Watch TV</p>');
 
         } else if(src == "images/television-256.png") {
-          $('.textbox').html('');
+          $('.textbox1').html('');
             $("#angry").attr("src","images/painter-256.png");
-            $('.textbox').append('<p class="floatright">Draw or Paint</p>');
+            $('.textbox1').append('<p class="floatright">Draw or Paint</p>');
 
         } else if(src == "images/painter-256.png") {
-          $('.textbox').html('');
+          $('.textbox1').html('');
             $("#angry").attr("src","images/singer-256.png");
-            $('.textbox').append('<p class="floatright">Sing With Them</p>');
+            $('.textbox1').append('<p class="floatright">Sing With Them</p>');
 
         } else if(src == "images/singer-256.png") {
-          $('.textbox').html('');
-        	$("#angry").attr("src","images/angry-256.png");
+          $('.textbox1').html('');
+        	$("#angry1").attr("src","images/angry-256.png");
         	alert("Call Help Line at 1866-531-2600");
 
         } else {
@@ -56,27 +56,27 @@
         var src = $('#harm').attr('src');
 
         if(src == 'images/bang-on-wall-256.png') {
-          $('.textbox').html('');
+          $('.textbox2').html('');
             $("#harm").attr("src","images/quiet-256.png");
-                $('.textbox').append('<p class="floatright">Go To Quiet Place</p>');
+                $('.textbox2').append('<p class="floatright">Go To Quiet Place</p>');
 
         } else if(src == 'images/quiet-256.png') {
-          $('.textbox').html('');
+          $('.textbox2').html('');
             $("#harm").attr("src","images/head-protection-256.png");
-            $('.textbox').html('Put on a Safety Helmet or Gloves!');
+            $('.textbox2').html('Put on a Safety Helmet or Gloves!');
 
         } else if(src == "images/head-protection-256.png") {
-          $('.textbox').html('');
+          $('.textbox2').html('');
             $("#harm").attr("src","images/hug-256.png");
-                $('.textbox').append('<p class="floatright">Gently Rock Back and Fourth</p>');
+                $('.textbox2').append('<p class="floatright">Gently Rock Back and Fourth</p>');
 
         } else if(src == "images/hug-256.png") {
-          $('.textbox').html('');
+          $('.textbox2').html('');
             $("#harm").attr("src","images/singer-256.png");
-                $('.textbox').append('<p class="floatright">Sing With Them</p>');
+                $('.textbox2').append('<p class="floatright">Sing With Them</p>');
 
         } else if(src == "images/singer-256.png") {
-          $('.textbox').html('');
+          $('.textbox2').html('');
         	$("#harm").attr("src","images/bang-on-wall-256.png");
             alert("Call help line at 1866 531 2600");
 
@@ -91,9 +91,9 @@
         var src = $('#suicide').attr('src');
 
         if(src == 'images/suicide-256.png') {
-          $('.textbox').html('');
+          $('.textbox3').html('');
             $("#suicide").attr("src","images/pills-256.png");
-                $('.textbox').append('<p class="floatright">Administer Medications</p>');
+                $('.textbox3').append('<p class="floatright">Administer Medications</p>');
         } else if(src == 'images/pills-256.png') {
             $("#suicide").attr("src","images/suicide-256.png");
             alert("Call Help Line at 416-408-4357");
@@ -107,13 +107,13 @@
         var src = $('#violent').attr('src');
 
         if(src == 'images/violent-256.png') {
-          $('.textbox').html('');
+          $('.textbox4').html('');
             $("#violent").attr("src","images/light-off-256.png");
-                $('.textbox').append('<p class="floatright">Take away sensory objects(Turn Off the Lights)</p>');
+                $('.textbox4').append('<p class="floatright">Take away sensory objects(Turn Off the Lights)</p>');
         } else if(src == 'images/light-off-256.png') {
-          $('.textbox').html('');
+          $('.textbox4').html('');
             $("#violent").attr("src","images/pills-256.png");
-                $('.textbox').append('<p class="floatright">Administer Medication</p>');
+                $('.textbox4').append('<p class="floatright">Administer Medication</p>');
 
         } else if (src == 'images/pills-256.png') {
 
