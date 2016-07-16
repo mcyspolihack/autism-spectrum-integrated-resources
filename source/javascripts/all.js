62cb702cae92ea4af50ca6ab6c08feb8c254170d
Impuls3iv <sammy.unterman@gmail.com>
2016-07-16 16:36:59 -0400

    completed text

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -35,10 +35,12 @@
             $('.textbox').append('<p class="floatright">Draw or Paint</p>');
 
         } else if(src == "images/painter-256.png") {
+          $('.textbox').html('');
             $("#angry").attr("src","images/singer-256.png");
-            $('#tips_box_one').append('<p class="floatright">Sing With Them</p>');
+            $('.textbox').append('<p class="floatright">Sing With Them</p>');
 
         } else if(src == "images/singer-256.png") {
+          $('.textbox').html('');
         	$("#angry").attr("src","images/angry-256.png");
         	alert("Call Help Line at 1866-531-2600");
 
@@ -54,20 +56,30 @@
         var src = $('#harm').attr('src');
 
         if(src == 'images/bang-on-wall-256.png') {
+          $('.textbox').html('');
             $("#harm").attr("src","images/quiet-256.png");
+                $('.textbox').append('<p class="floatright">Go To Quiet Place</p>');
 
         } else if(src == 'images/quiet-256.png') {
+          $('.textbox').html('');
             $("#harm").attr("src","images/head-protection-256.png");
+            $('.textbox').html('Put on a Safety Helmet or Gloves!');
 
         } else if(src == "images/head-protection-256.png") {
+          $('.textbox').html('');
             $("#harm").attr("src","images/hug-256.png");
+                $('.textbox').append('<p class="floatright">Gently Rock Back and Fourth</p>');
 
         } else if(src == "images/hug-256.png") {
+          $('.textbox').html('');
             $("#harm").attr("src","images/singer-256.png");
+                $('.textbox').append('<p class="floatright">Sing With Them</p>');
 
         } else if(src == "images/singer-256.png") {
+          $('.textbox').html('');
         	$("#harm").attr("src","images/bang-on-wall-256.png");
             alert("Call help line at 1866 531 2600");
+
 
         } else {
             $("#harm").attr("src","images/bang-on-wall-256.png");
@@ -79,7 +91,9 @@
         var src = $('#suicide').attr('src');
 
         if(src == 'images/suicide-256.png') {
+          $('.textbox').html('');
             $("#suicide").attr("src","images/pills-256.png");
+                $('.textbox').append('<p class="floatright">Administer Medications</p>');
         } else if(src == 'images/pills-256.png') {
             $("#suicide").attr("src","images/suicide-256.png");
             alert("Call Help Line at 416-408-4357");
@@ -93,11 +107,16 @@
         var src = $('#violent').attr('src');
 
         if(src == 'images/violent-256.png') {
+          $('.textbox').html('');
             $("#violent").attr("src","images/light-off-256.png");
+                $('.textbox').append('<p class="floatright">Take away sensory objects(Turn Off the Lights)</p>');
         } else if(src == 'images/light-off-256.png') {
+          $('.textbox').html('');
             $("#violent").attr("src","images/pills-256.png");
+                $('.textbox').append('<p class="floatright">Administer Medication</p>');
 
         } else if (src == 'images/pills-256.png') {
+
             $("#violent").attr("src","images/violent-256.png");
             alert("Call Help Line at 1866-531-2600");
         } else {
