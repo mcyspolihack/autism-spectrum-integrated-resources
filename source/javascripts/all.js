75ac28e9d1ac39113d9dbf60d263844934ab3a45
andklar <andrewklar@hotmail.com>
2016-07-16 16:09:37 -0400

    jquery2

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -28,8 +28,8 @@
             $("#angry").attr("src","images/singer-256.png"); 
 
         } else if(src == "images/singer-256.png") {
-        	$('#angry').html("");
-            $("#angry").append('#help1'); 
+        	$("#angry").attr("src","images/angry-256.png");
+        	alert("Call Help Line at 1866-531-2600");
 
         } else {
             $("#angry").attr("src","images/quiet-256.png");
@@ -55,12 +55,27 @@
             $("#harm").attr("src","images/singer-256.png"); 
 
         } else if(src == "images/singer-256.png") {
-            $("#harm").attr("src","images/bang-on-wall-256.png"); 
+        	$("#harm").attr("src","images/bang-on-wall-256.png");
+            alert("Call help line at 1866 531 2600");
 
         } else {
             $("#harm").attr("src","images/bang-on-wall-256.png");
         }
     }); 
+
+    $("#suicide").click(function() {
+
+        var src = $('#suicide').attr('src');
+
+        if(src == 'images/suicide-256.png') {
+            $("#suicide").attr("src","images/pills-256.png");
+        } else if(src == 'images/pills-256.png') {
+            $("#suicide").attr("src","images/suicide-256.png");
+            alert("Call Help Line at 416-408-4357");
+        } else {
+            $("#suicide").attr("src","images/violent-256.png");
+        }
+    });  
 
     $("#violent").click(function() {
 
@@ -73,7 +88,7 @@
 
         } else if (src == 'images/pills-256.png') {
             $("#violent").attr("src","images/violent-256.png");
-
+            alert("Call Help Line at 1866-531-2600");
         } else {
             $("#violent").attr("src","images/violent-256.png");
         }
